e6c243753ff4005349d426c7b3ab428780ed328e
abdelghafarabdelmordy <33281617+abdelghafarabdelmordy@users.noreply.github.com>
2024-02-21 20:55:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,8 +26,8 @@ resource "aws_security_group" "main" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port   = 88
-    to_port     = 88
+    from_port   = 8855
+    to_port     = 8855
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
