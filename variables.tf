511056b32f169045c6f0d7958f5c4c24f17cef3e
abdelghafarabdelmordy <33281617+abdelghafarabdelmordy@users.noreply.github.com>
2024-05-17 07:23:03 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,22 +1,22 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "region" {
-  default = "us-east-1"
-}
+#variable "aws_access_key" {}
+#variable "aws_secret_key" {}
+#variable "region" {
+#  default = "us-east-1"
+#}
 
-variable "vpc_cidr_block" {
-  default = "10.0.0.0/16"
-}
+#variable "vpc_cidr_block" {
+#  default = "10.0.0.0/16"
+#}
 
-variable "subnet_cidr_blocks" {
-  type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
-}
+#variable "subnet_cidr_blocks" {
+#  type    = list(string)
+ # default = ["10.0.1.0/24", "10.0.2.0/24"]
+#}
 
-variable "availability_zones" {
-  type    = list(string)
-  default = ["us-east-1a", "us-east-1b"]
-}
+#variable "availability_zones" {
+#  type    = list(string)
+#  default = ["us-east-1a", "us-east-1b"]
+#}
 
 
 
